39d35eec951a69e9b5c6ce23d1a5d4abc349f8e7
voß <lueko.voss@gmx.de>
2020-08-06 17:46:22 +0200

    Create spring boot project;

---
+++ mediahand/src/main/java/com/intege/mediahand/MediahandApplication.java
@@ -0,0 +1,13 @@
+package com.intege.mediahand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediahandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediahandApplication.class, args);
+	}
+
+}
---
+++ mediahand/src/test/java/com/intege/mediahand/MediahandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intege.mediahand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediahandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
